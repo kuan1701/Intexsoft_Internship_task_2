a64d789a79ea9d818281f70b046db07af929ef9e
kuan1701 <kuanchin17011993@gmail.com>
2021-09-06 10:27:52 +0300

    changed some files

--- config/db.config.js
+++ config/db.config.js
@@ -3,19 +3,19 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'customers',
+    database: 'books',
     password: 'Vwpassatb5.5',
     port: '5432'
 });
 
 // получение списка пользователей
 const getCustomers = (req, res) => {
-    const query = "SELECT * FROM customer";
+    const query = "SELECT * FROM book";
 
     pool.query(query, function(err, data) {
         if(err) return console.log(err);
         res.render("index.hbs", {
-            users: data
+            books: data
         });
       });
 }
@@ -27,12 +27,12 @@ const createCustomerForm = (req, res) => {
 
 // получаем отправленные данные и добавляем их в БД 
 const createCustomer = (req, res) => {
-    const query = "INSERT INTO customer (name, age) VALUES ($1, $2)";
+    const query = "INSERT INTO book (book_name, book_lenght) VALUES ($1, $2)";
 
     if(!req.body) return res.sendStatus(400);
-    const name = req.body.name;
-    const age = req.body.age;
-    pool.query(query, [name, age], function(err, data) {
+    const book_name = req.body.book_name;
+    const book_lenght = req.body.book_lenght;
+    pool.query(query, [book_name, book_lenght], function(err, data) {
       if(err) return console.log(err);
       res.redirect("/");
     });
@@ -40,13 +40,13 @@ const createCustomer = (req, res) => {
 
 // получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
 const editCustomerInfoForm = (res, req) => {
-    const query = "SELECT * FROM customer WHERE id = $1";
+    const query = "SELECT * FROM book WHERE book_id = $1";
 
-    const id = req.params.id;
-    pool.query(query, [id], function(err, data) {
+    const book_id = req.params.book_id;
+    pool.query(query, [book_id], function(err, data) {
     if(err) return console.log(err);
     res.render("edit.hbs", {
-        user: data[0]
+        book: data[0]
     });
   });
 }
@@ -54,12 +54,12 @@ const editCustomerInfoForm = (res, req) => {
 // получаем отредактированные данные и отправляем их в БД
 const editCustomer = (res, req) => {
     if(!req.body) return res.sendStatus(400);
-    const name = req.body.name;
-    const age = req.body.age;
-    const id = req.body.id;
-    const query = "UPDATE customer SET name = $1, age = $2 WHERE id = $3";
+    const book_name = req.body.book_name;
+    const book_lenght = req.body.book_lenght;
+    const book_id = req.body.book_id;
+    const query = "UPDATE book SET book_name = $1, book_lenght = $2 WHERE book_id = $3";
 
-    pool.query(query, [name, age, id], function(err, data) {
+    pool.query(query, [book_name, book_lenght, book_id], function(err, data) {
     if(err) return console.log(err);
     res.redirect("/");
   });
@@ -67,10 +67,10 @@ const editCustomer = (res, req) => {
 
 // получаем id удаляемого пользователя и удаляем его из бд
 const deleteCustomer = (res, req) => {
-    const id = req.params.id;
-    const query = "DELETE FROM customer WHERE id = $1";
+    const book_id = req.params.book_id;
+    const query = "DELETE FROM book WHERE book_id = $1";
 
-    pool.query(query, [id], function(err, data) {
+    pool.query(query, [book_id], function(err, data) {
     if(err) return console.log(err);
     res.redirect("/");
   });
